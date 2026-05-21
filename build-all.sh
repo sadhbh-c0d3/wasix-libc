3546d202982304196c48a0a262515d9e714eb35e
Arshia <arshia001@live.com>
2026-05-21 19:25:35 +0400

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -6,11 +6,16 @@ export CHECK_SYMBOLS=no
 
 make clean-all
 ./build32.sh
-make clean
 ./build32-eh.sh
-make clean
+./build32-exnref-eh.sh
 ./build32-ehpic.sh
+./build32-exnref-ehpic.sh
 
+mkdir -p ~/.wasix-sysroot
 rsync -Lrtv --delete ./sysroot32/ ~/.wasix-sysroot/sysroot
 rsync -Lrtv --delete ./sysroot32-eh/ ~/.wasix-sysroot/sysroot-eh
+rsync -Lrtv --delete ./sysroot32-eh/ ~/.wasix-sysroot/sysroot-exnref-eh
 rsync -Lrtv --delete ./sysroot32-ehpic/ ~/.wasix-sysroot/sysroot-ehpic
+rsync -Lrtv --delete ./sysroot32-ehpic/ ~/.wasix-sysroot/sysroot-exnref-ehpic
+
+echo Done!
